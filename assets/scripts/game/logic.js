cea1e2046846065ac2a6155038c09b992ef91560
ripleyofvenus <ripleyofvenus@gmail.com>
2017-09-10 23:41:33 -0400

    Game Registers Wins & Draws
    Next step to make the game board accept no more moves after a win, and
    clear the board.

--- assets/scripts/game/logic.js
+++ assets/scripts/game/logic.js
@@ -5,9 +5,7 @@ const game = require('./game')
 const app = require('../app')
 const events = require('./events')
 
-let turn = 'X'
 let gameBoardArray = game.gameBoardArray
-let hasWin = false
 let hasDraw = false
 
 const gameReset = () => {
@@ -27,65 +25,51 @@ const move = function (event) {
     $('[data-id=' + id + ']').html('<span>X</span>')
     game.gameBoardArray[parseInt(id)] = 'X'
     checkDraw()
+    checkWin()
     game.xMove = false
   } else if (isValidMove === true && game.xMove === false) {
     $('[data-id=' + id + ']').html('<span>O</span>')
     game.gameBoardArray[parseInt(id)] = 'O'
     checkDraw()
+    checkWin()
     game.xMove = true
   } else if (isValidMove !== true) {
     console.log('try another box')
     // ui.moveFailure(error)
     console.log(game.gameBoardArray)
   }
-  //
-  // const winner = game.checkWin()
-  // if (winner) {
-  //   console.log('When you play against yourself, you always win')
-  //   // gameOver = true
-  // } else {
-  //   if (game.checkDraw(game.gameBoardArray)) {
-  //     console.log('If nobody loses, who wins?')
-  //     // gameOver = true
-  //   }
-  // }
 }
 
-// game.game.prototype.checkForDraw = function () {
-//   // game is over when all boxes are full
-//   return this.box.join('').length === 9
-// }
-//
-// const checkRow = function (a, b, c, turn) {
-//   console.log('checkRow')
-//   let result = false
-//   if (gameBoardArray[a] === turn && gameBoardArray[b] === turn &&
-//     gameBoardArray[c] === turn) {
-//     result = true
-//   }
-//   return result
-// }
+const checkRow = function (a, b, c, currentmove) {
+  console.log('checkrow1')
+  let result = false
+  const id = event.target.attributes[1].value
+  let currentMove = game.gameBoardArray[parseInt(id)]
+  if (gameBoardArray[a] === currentMove && gameBoardArray[b] === currentMove &&
+    gameBoardArray[c] === currentMove) {
+    result = true
+  }
+  return result
+}
 
-// const checkWin = function (turn) {
-//   console.log('checkWin()')
-//   // console.log(gameArray)
-//   // console.log(turn)
-//   let result = false
-//   if (checkRow(0, 1, 2, turn) ||
-//      checkRow(3, 4, 5, turn) ||
-//      checkRow(6, 7, 8, turn) ||
-//      checkRow(0, 3, 6, turn) ||
-//      checkRow(1, 4, 7, turn) ||
-//      checkRow(2, 5, 8, turn) ||
-//      checkRow(0, 4, 8, turn) ||
-//      checkRow(2, 4, 6, turn) ||
-//      checkRow(0, 4, 8, turn)) {
-//     result = true
-//     hasWin = true
-//     console.log('When you play against yourself, you always win')
-//   }
-//   return updateGame
-// }
+const checkWin = function (currentmove) {
+  let result = false
+  const id = event.target.attributes[1].value
+  let currentMove = game.gameBoardArray[parseInt(id)]
+  if (checkRow(0, 1, 2, currentMove) ||
+     checkRow(3, 4, 5, currentMove) ||
+     checkRow(6, 7, 8, currentMove) ||
+     checkRow(0, 3, 6, currentMove) ||
+     checkRow(1, 4, 7, currentMove) ||
+     checkRow(2, 5, 8, currentMove) ||
+     checkRow(0, 4, 8, currentMove) ||
+     checkRow(2, 4, 6, currentMove) ||
+     checkRow(0, 4, 8, currentMove)) {
+    result = true
+    console.log('When you play against yourself, you always win')
+  }
+  return result
+}
 
 const checkDraw = function () {
   console.log('checkDraw works')
@@ -104,10 +88,9 @@ const updateGame = (data) => {
 }
 
 module.exports = {
-  turn,
   move,
-  // checkRow,
-  // checkWin,
+  checkRow,
+  checkWin,
   checkDraw,
   updateGame,
   gameReset
